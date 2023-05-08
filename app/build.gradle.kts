d9a0b6636432b777417af8fc297dc323e6064825
Daniel <dleguizamon@merqueo.com>
2023-05-08 16:40:13 -0500

    solved Camilo's comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,12 +78,6 @@ dependencies {
     implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
-// Mockk
-    testImplementation("io.mockk:mockk:1.13.5")
-    androidTestImplementation("io.mockk:mockk-android:1.13.5")
-    implementation("io.mockk:mockk-android:1.13.5")
-    implementation("io.mockk:mockk-agent-android:1.13.5")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
